963edc4669c98aceaeb86ed22589e67c2f4c05ca
Bennie Robinson <brobinson88@gmail.com>
2020-07-14 08:09:30 -0400

    Enhance router to direct resources to controllers

--- app/router.rb
+++ app/router.rb
@@ -7,16 +7,62 @@ class Router
   end
 
   def route!
-    if @request.path == '/'
-      [200, { 'Content-Type' => 'text/plain' }, ['Hello World']]
-    else
-      not_found
+    if (klass = controller_class)
+      add_route_info_to_request_params!
+
+      controller = klass.new(@request)
+      action = route_info[:action]
+
+      if controller.respond_to?(action)
+        puts "\nRouting to #{klass}##{action}"
+        return controller.public_send(action)
+      end
     end
+
+    not_found
   end
 
   private
 
   def not_found(msg = 'Not Found')
     [404, { 'Content-Type' => 'text/plain' }, [msg]]
+  end
+
+  def controller_class
+    Object.const_get(controller_name)
+  rescue NameError => _e
+    nil
+  end
+
+  def add_route_info_to_request_params!
+    @request.params.merge!(route_info)
+  end
+
+  def controller_name
+    "#{route_info[:resource].capitalize}Controller"
+  end
+
+  def route_info
+    @route_info ||= begin
+      resource = path_fragments[0] || 'base'
+      id, action = find_id_and_action(path_fragments[1])
+      { resource: resource, action: action, id: id }
+    end
+  end
+
+  def path_fragments
+    @path_fragments ||= @request.path.split('/').reject(&:empty?)
+  end
+
+  def find_id_and_action(fragment)
+    case fragment
+    when 'new'
+      [nil, :new]
+    when nil
+      action = @request.get? ? :index : :create
+      [nil, action]
+    else
+      [fragment, :show]
+    end
   end
 end
